4cc48e656116d9fff150eaefcaa3f9d1fd3bed76
sucinursabila <sucinrsabila@gmail.com>
2024-04-02 09:51:29 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,12 @@
+print("## PROGRAM PYTHON MENGHITUNG LUAS SEGITIGA ##")
+print("=============================================")
+print()
+
+def hitungluasSegitiga(a,t):
+    return round( 0.5 a * t,2)
+
+alas = float(input("input alas segitiga"))
+tinggi = float(input("input tinggi segitiga"))
+
+print('luas segitiga=', hitungluasSegitiga(a;alas,tinggi))
+print()
